e85c9420e737c225ed22b36ca77981050b63bc83
Nadezhda Yuzhakova <n.a.manuylova@gmail.com>
2024-06-06 11:29:47 +0100

    Adding repeat 'guess_number' game and set default max number for generation.

--- python_course_1/guess_number.py
+++ python_course_1/guess_number.py
@@ -35,16 +35,19 @@ def game_loop(nmb: int):
             print(f"You are right! The number is {nmb}. Congratulations!"
                   f"\nYou guessed the number in {cnt} attempts")
             break
+    repeat = input("Enter 'y' to play again. ")
+    if repeat == 'y':
+        main()
 
 
 def main():
     print("Hello! Let's guess the number?"
           "\nEnter 'q' if you want to quit the game.")
 
-    max_number = input("Enter the maximum number for generation:")
+    max_number = input("Enter the maximum number for generation: ")
     if not max_number.isdigit():
-        print("Shit, goodbye :((")
-        return
+        print("Invalid value. Default maximum number 100 is taken.")
+        max_number = 100
 
     game_loop(generate_num(int(max_number)))
 
