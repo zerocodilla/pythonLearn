7866b4ea97f1d678ed13dc8f22c04bcebb8111b4
Nadezhda Yuzhakova <n.a.manuylova@gmail.com>
2024-06-14 11:38:56 +0100

    Adding guess_word working version

---
+++ python_course_1/guess_word.py
@@ -0,0 +1,130 @@
+from random import *
+
+word_list = ['pineapple', 'car', 'tour']
+
+
+def get_word():
+    return choice(word_list).upper()
+
+
+# current hangman state
+def display_hangman(tries):
+    stages = [  # final stage: head, body, both hands, both legs
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                ''',
+                # head, body, both hands, one leg
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                ''',
+                # head, body, both hands
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                ''',
+                # head, body, one hand
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                ''',
+                # head, body
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                ''',
+                # head
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                ''',
+                # initial state
+                '''
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                '''
+    ]
+    return stages[tries]
+
+
+def play(word):
+    word_completion = ['_' for i in word] # word pattern
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    print("Let's guess the word!")
+    print(display_hangman(tries))
+    print(*word_completion, sep='')
+
+    while not guessed:
+        s = input("Enter a letter or a word to guess: ").upper()
+        while not s.isalpha():
+            s = input("Unexpected result. Please enter a letter or a word to guess: ").upper()
+        while s in guessed_letters or s in guessed_words:
+            s = input("You already tried this. Please enter a letter or a word you didn't try: ").upper()
+
+        if len(s) == 1:
+            guessed_letters.append(s)
+            tries -= 1
+
+            for letter in guessed_letters:
+                for i in range(len(word)):
+                    if letter == word[i]:
+                        word_completion[i] = letter
+            print(*word_completion, sep='')
+        else:
+            guessed_words.append(s)
+            tries -= 1
+            if s == word:
+                word_completion = s
+
+        if ''.join(word_completion) == word:
+            print("Congratulations! You guessed the word.")
+            break
+        if tries == 0:
+            print(f"You have used all tries. The word was {word}.")
+            break
+
+
+# if __name__ in '__main__':
+#     main()
+
+
+play(get_word())
